897f44534b9227c4442a1aac9f7057dc3f7da301
DJS-GH <52869559+DJS-GH@users.noreply.github.com>
2019-09-24 21:40:07 +0200

    Add files via upload

---
+++ EuroSkin/__resource.lua
@@ -0,0 +1,7 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+name 'DDT_EuroSkin'
+description 'Euro Currency ipv Dollar'
+author 'DJS - DDT ~Dutch Development Team~'
+version 'v1.0'
+url 'https://discord.gg/xNh3TpS'
